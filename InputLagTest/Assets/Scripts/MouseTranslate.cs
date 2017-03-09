941723a1e134040f15d70f05a6a208bca8a51e43
HiddenMonk <unityhiddenmonk@gmail.com>
2017-03-08 23:10:41 -0500

    Add files via upload

---
+++ InputLagTest/Assets/Scripts/MouseTranslate.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+//This is placed on the cameras gameobject
+public class MouseTranslate : MonoBehaviour
+{
+	public static float mouseSpeed;
+	public static bool useButtonInsteadOfMouseMove;
+	public static KeyCode inputKey = KeyCode.Mouse0;
+
+	void Update()
+	{
+		if(useButtonInsteadOfMouseMove) //Could be keyboard or mouse buttons
+		{
+			if(Input.GetKeyDown(inputKey))
+			{
+				transform.Rotate(0, mouseSpeed, 0);
+			}
+		}else{
+			transform.Rotate(0, Input.GetAxisRaw("Mouse X") * mouseSpeed, 0);
+		}
+	}
+}
